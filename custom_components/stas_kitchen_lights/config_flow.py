26f63209079e50afb0b5d6fe20ba404eadf72891
slavengorama <slave@SLAVENGORAMA>
2023-07-16 10:25:56 +0300

    change config flow

--- custom_components/stas_kitchen_lights/config_flow.py
+++ custom_components/stas_kitchen_lights/config_flow.py
@@ -1,5 +1,19 @@
 from homeassistant import config_entries
+import voluptuous as vol
 
-class ExampleConfigFlow(config_entries.ConfigFlow, domain='stas_kitchen_lights'):
+DATA_SCHEMA = vol.Schema({("ip"): str})
+
+
+
+class ConfigFlow(config_entries.ConfigFlow, domain='stas_kitchen_lights'):
 
     VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+
+        errors = {}
+
+        # If there is no user input or there were errors, show the form again, including any errors that were found with the input.
+        return self.async_show_form(
+            step_id="ip", data_schema=DATA_SCHEMA, errors=errors
+        )
